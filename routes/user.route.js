6aa47d930ab28d2cb5b64acf7248bb5218090577
Vinicius Lima <viniciuslimadamata@gmail.com>
2019-04-06 14:01:59 -0400

    cors removido

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,15 +1,15 @@
 const controller = require("../controllers/user.controller");
 const express = require("express");
-const cors = require("cors");
-const corsConfig = cors({
-   origin: "https://cadastroabe.netlify.com/#"
-});
+// const cors = require("cors");
+// const corsConfig = cors({
+//    origin: "https://cadastroabe.netlify.com/#"
+// });
 const router = express.Router();
-router.get("/", corsConfig, controller.allUsers);
-router.post("/", corsConfig, controller.saveUser);
-router.get("/:id", corsConfig, controller.findUser);
-router.patch("/:id", corsConfig, controller.updatePassword);
-router.delete("/:id", corsConfig, controller.deleteUser);
-router.put("/all/:id", corsConfig, controller.updateAll);
+router.get("/", controller.allUsers);
+router.post("/", controller.saveUser);
+router.get("/:id", controller.findUser);
+router.patch("/:id", controller.updatePassword);
+router.delete("/:id", controller.deleteUser);
+router.put("/all/:id", controller.updateAll);
 
 module.exports = router;
