27e1b2336e6e8332bc050624f0d67128af75e07d
Vinicius Lima <viniciuslimadamata@gmail.com>
2019-04-09 21:54:50 -0400

    email fix

--- middleware/cors.middleware.js
+++ middleware/cors.middleware.js
@@ -1,5 +1,5 @@
 function cors(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "https://cadastroabe.netlify.com");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 }
